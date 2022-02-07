0cb0a024409429e1c1d393c9d7267d874e97372b
makayasa <rifqimakayasa@gmail.com>
2022-02-07 22:47:36 +0700

    entity

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,5 +1,12 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
 export class User {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     email: string;
+
+    @Column()
     password: string;
 }
